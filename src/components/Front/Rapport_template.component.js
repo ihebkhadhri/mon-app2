0f283e394f4c6f89e5a4e6559876435f78f3e400
RamiJammeli <60295241+RamiJammeli@users.noreply.github.com>
2022-05-16 21:50:45 +0100

    Gérer config

--- src/components/Front/Rapport_template.component.js
+++ src/components/Front/Rapport_template.component.js
@@ -1,17 +1,59 @@
 import DocViewer, { DocViewerRenderers } from "react-doc-viewer";
+import axios from 'axios';
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
 
-export default function Rapport_template() {
-  const docs = [
+
+
+export default class Rapport_template extends React.Component  {
+
+  state = {
+    docs: []
+  }
+
+
+  componentDidMount() {
+    
+    axios.get(`https://localhost:7103/Template/AllTemplatesByCategorie/`)
+      .then(res => {
+       
+        const _doc = [
+          { uri: require("D:/pfe/Front/mon-app/src/Templates Word_pdf/a.pdf") },
+          
+        ];
+
+        const _docs=[];
+        _docs.push(_doc);
+
+        console.log(_docs);
+
+        this.setState({
+          docs:_docs
+        }
+        )
+
+        
+
+      })
+  
+    }
+
+ /* const docs = [
     { uri: require("../../../src/a.pdf") },
     
-  ];
+  ];*/
 
+  render() {
   return (
-    <div className="aa">
-      <h1>elee</h1>
-      <DocViewer
+    <div className="templates">
+      
+      <div className="row">
+        
+      { this.state.docs.map(doc =>
+      <div className="col-3">
+        <DocViewer
         pluginRenderers={DocViewerRenderers}
-        documents={docs}
+        documents={doc}
         config={{
           header: {
             disableHeader: false,
@@ -19,8 +61,14 @@ export default function Rapport_template() {
             retainURLParams: false
           }
         }}
-        style={{ height: 1000 }}
+        style={{ height: 500 }}
       />
+</div>
+        )}
+
+
+</div>
     </div>
   );
+}
 }
