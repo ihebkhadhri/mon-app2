2955516aed013f4a2ac7ef140063318f4e9128ca
eleeswaidi <elee.swaidi@esprit.tn>
2022-06-21 22:04:15 +0100

    extraire donnee

--- src/components/Front/ArchiveClient.js
+++ src/components/Front/ArchiveClient.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 import $ from 'jquery';
+import swal from 'sweetalert';
 
 import './../../jquery.dataTables.min.css'
 
@@ -12,6 +13,9 @@ export default class ArchiveClient extends React.Component {
     constructor(props) {
         super(props);
         this.delete = this.delete.bind(this);
+        this.JSalert=this.JSalert.bind(this);
+        this.downloadinput=this.downloadinput.bind(this);
+        
     }
 
     state = {
@@ -19,6 +23,55 @@ export default class ArchiveClient extends React.Component {
        
          archives: []
     
+      }
+
+      JSalert(id){
+        
+        swal( {
+          title: "Confirmez-vous?",
+          text: "Voulez-vous extraire les données du rapport pour en faire un nouveau rapport ou Télécharger les données",
+          icon: "warning",
+          buttons: {
+            extraire: " Extraire les données",
+            catch: {
+              text: "Télécharger",
+              value: "Telecharger",
+            },
+            Annuler: "Annuler",
+          },
+        })
+        .then((value) => {
+          switch (value) {
+         
+            case "extraire":
+              swal("Pikachu fainted! You gained 500 XP!");
+              break;
+         
+            case "Telecharger":
+              this.downloadinput(id);
+              break;
+         
+            default :
+              swal("Got away safely!");
+          }
+        });
+      }
+
+      downloadinput(id){
+        axios.get(`https://localhost:7103/Archive/Downloadinput/`+id)
+        .then(res => { 
+
+          console.log(res.data);
+          const url = 'data:xml;base64,'+ encodeURI(res.data);
+          const link = document.createElement('a');
+          link.href = url;
+          link.setAttribute('download', 'input.xml'); //or any other extension
+          document.body.appendChild(link);
+          link.click();
+         document.body.removeChild(link);
+       
+      }
+        )
       }
 
       download(id){
@@ -86,7 +139,9 @@ export default class ArchiveClient extends React.Component {
                                 <th>
                                     <button className="btn-link" onClick={() => this.download(item.id)}><i className="fas fa-download"></i></button>
                                     <button className="btn-link" onClick={() =>this.delete(item.id)}><i className="fas fa-trash-alt" style={{ color:"red"}}></i></button>
+                                    <button  onClick={() => this.JSalert(item.id)}><i class="fas fa-file-import"></i></button>
                                     </th>
+
                             </tr>
                         )
                     })}
